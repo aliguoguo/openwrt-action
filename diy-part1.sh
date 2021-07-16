f87075244bff77ce4e128e8af752ec6a345beac5
aliguoguo <66200481+aliguoguo@users.noreply.github.com>
2021-07-16 12:02:01 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 sed -i '$a src-git vernesong https://github.com/vernesong/OpenClash' feeds.conf.default
+git clone https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
